d65e27bce23c71fb11040fe80305cd02607c84a2
Vitor Leone Cezar <vleone.social@gmail.com>
2021-03-04 22:47:25 -0300

    funcionalidade de exibir só a geração escolhida

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
-const generatePokemonPromises = () => Array(151).fill().map((_, index) =>
+const pokeQuantity = 898
+
+const generatePokemonPromises = () => Array(pokeQuantity).fill().map((_, index) =>
   fetch(getPokemonUrl(index + 1)).then(response => response.json()))
 
 const generateHTML = pokemons => pokemons.reduce((acc, { name, id, types }) => {
@@ -22,9 +24,54 @@ const insertPokemonsIntoPage = pokemons => {
   ul.innerHTML = pokemons
 }
 
+async function init (first, last) {
+  const pokemonPromises = generatePokemonPromises()
 
-const pokemonPromises = generatePokemonPromises()
+  Promise.all(pokemonPromises.slice(first, last))
+    .then(generateHTML)
+    .then(insertPokemonsIntoPage)
+}
 
-Promise.all(pokemonPromises)
-  .then(generateHTML)
-  .then(insertPokemonsIntoPage)
+init(0, 151)
+
+document.querySelector('#generations')
+  .addEventListener('change', event => {
+    const choosenOption = event.target.value
+
+    switch (choosenOption) {
+      case 'first':
+        init(0, 151)
+        break;
+    
+      case 'second':
+        init(151, 251)
+        break;
+    
+      case 'third':
+        init(251, 386)
+        break;
+    
+      case 'fourth':
+        init(386, 493)
+        break;
+    
+      case 'fifth':
+        init(493, 649)
+        break;
+    
+      case 'sixth':
+        init(649, 721)
+        break;
+    
+      case 'seventh':
+        init(721, 809)
+        break;
+    
+      case 'eighth':
+        init(809, 898)
+        break;
+    
+      default:
+        break;
+    }
+  })
